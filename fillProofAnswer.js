4946055f4f162fc7599ba88ac5c591bf9b6be3dc
levee98 <leventet@t-email.hu>
2020-05-26 19:35:23 +0200

    add rownumber to the table

--- fillProofAnswer.js
+++ fillProofAnswer.js
@@ -1,7 +1,8 @@
 const ground = require('./ground');
 
 // filling the guessed code in row 1
-const fillProofedAnswer = (blk, wht) => {
+const fillProofedAnswer = (blk, wht, live) => {
+  let digitNull = '';
   const whit = wht - blk;
   for (let j = 0; j < blk; j++) {
     ground.playboard[1][j + 5] = 2;
@@ -11,6 +12,13 @@ const fillProofedAnswer = (blk, wht) => {
   }
   for (let j = 0; j < 4 - (blk + whit); j++) {
     ground.playboard[1][j + blk + whit + 5] = ' ';
+  }
+  if (live > 2) {
+    const colWid = 12 - live;
+    digitNull = '0' + colWid;
+    ground.playboard[1][4] = digitNull;
+  } else {
+    ground.playboard[1][4] = 12 - live;
   }
 };
 
