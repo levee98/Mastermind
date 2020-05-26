694b66ac95267426bfd9b3d587ee7763ffa1a694
levee98 <leventet@t-email.hu>
2020-05-26 19:52:56 +0200

    add checking of the allowed digits

--- inputGuess.js
+++ inputGuess.js
@@ -1,16 +1,34 @@
 const readline = require('readline-sync');
+const separateDigits = require('./separateDigits');
+const isGuessedValid = require('./isGuessedVaild');
+const axel = require('axel');
 
 // asking user for guessed code
-const guessedCode = () => {
+const guessedCode = (hMD) => {
   while (true) {
-    const guessed = readline.questionInt('Kérem a 4 jegyű kódodat! ');
-    console.log('Ezt írtad be: ', guessed);
+    axel.text(50, 5, '                                '); // clear the line
+    axel.text(50, 5, 'Kérem a 4 jegyű kódodat!');
+    const guessed = readline.questionInt(' ');
+    //    console.log('Ezt írtad be: ', guessed);
     const guess = guessed.toString();
-    console.log(guessed, guess.length);
+    // axel.text(50, 7, 'Beírt: ', guessed, guess.length);
     if (guess.length === 4) {
-      return guessed;
+      const sepped = separateDigits.digits(guessed); // a vélt kód szétbontása
+      const validGuessed = isGuessedValid.isValid(sepped, hMD);
+      if (!validGuessed) {
+        return sepped;
+      } else {
+        axel.fg(255, 0, 0);
+        axel.text(50, 7, 'Nem a feltételeknek megfelelő számot adtál meg!');
+        axel.text(50, 9, 'Kérem a lehetséges számjegyekből álló');
+        axel.text(50, 11, '4 jegyű számot adjon meg!');
+        axel.fg(255, 255, 255);
+      }
     } else {
-      console.log('Kérem 4 jegyű számot adjon meg!');
+      axel.fg(255, 0, 0);
+      axel.text(50, 9, 'Kérem a lehetséges számjegyekből álló');
+      axel.text(50, 11, '4 jegyű számot adjon meg!');
+      axel.fg(255, 255, 255);
     }
   }
 };
