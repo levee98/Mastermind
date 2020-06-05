738f5fb49aa4f8397671bd8a7eb5f90d17e99a3d
levee98 <leventet@t-email.hu>
2020-06-05 08:04:28 +0200

    printouts printout-positions changes

--- gameOver.js
+++ gameOver.js
@@ -1,36 +1,38 @@
 const readline = require('readline-sync');
 const axel = require('axel');
 
-const win = (genCo) => {
+const win = (genCo, xMiddle) => {
+  const rigthMiddle = ((xMiddle + 27) + xMiddle * 2) / 2;
   axel.bg(255, 0, 0);
-  axel.box(94, 1, 32, 9);
-  axel.text(103, 2, 'Congratulation!');
-  axel.text(100, 4, ':) Y O U   W I N ! :)');
-  axel.text(95, 6, 'You found out the combination!');
+  axel.box(rigthMiddle - 16, 1, 32, 9);
+  axel.text(rigthMiddle - 8, 2, 'Congratulation!');
+  axel.text(rigthMiddle - 10, 4, ':) Y O U   W I N ! :)');
+  axel.text(rigthMiddle - 15, 6, 'You found out the combination!');
   axel.fg(255, 255, 255);
-  axel.text(103, 8, '');
+  axel.text(rigthMiddle - 6, 8, '');
   console.log(genCo);
   axel.bg(0, 255, 0);
   axel.text(0, 30, '');
   console.log();
-  axel.text(7, 30, '');
+  axel.text(rigthMiddle - 10, 30, '');
   const key = readline.keyInPause();
   console.clear();
 };
 
-const loose = (genCo) => {
+const loose = (genCo, xMiddle) => {
+  const rigthMiddle = ((xMiddle + 27) + xMiddle * 2) / 2;
   axel.bg(255, 0, 0);
-  axel.box(97, 1, 27, 10);
-  axel.text(106, 2, 'Game Over!');
-  axel.text(99, 4, 'You have no more lives!');
-  axel.text(98, 5, 'The rigth color and place');
-  axel.text(103, 7, 'combination was:');
-  axel.text(104, 9, '');
+  axel.box(rigthMiddle - 14, 1, 27, 10);
+  axel.text(rigthMiddle - 5, 2, 'Game Over!');
+  axel.text(rigthMiddle - 12, 4, 'You have no more lives!');
+  axel.text(rigthMiddle - 13, 5, 'The rigth color and place');
+  axel.text(rigthMiddle - 8, 7, 'combination was:');
+  axel.text(rigthMiddle - 6, 8, '');
   console.log(genCo);
-  axel.fg(255, 0, 0);
+  axel.bg(0, 255, 0);
   axel.text(0, 30, '');
   console.log();
-  axel.text(7, 30, '');
+  axel.text(rigthMiddle - 10, 30, '');
   const key = readline.keyInPause();
   console.clear();
 };
