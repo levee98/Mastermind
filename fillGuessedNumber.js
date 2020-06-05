49513d2474cc5ae9c89c69d561e3a3088c2beba8
levee98 <leventet@t-email.hu>
2020-06-05 07:40:48 +0200

    change const-name

--- fillGuessedNumber.js
+++ fillGuessedNumber.js
@@ -1,10 +1,10 @@
 const ground = require('./ground');
 
 // filling the guessed code in row 1
-const fillGuessedCode = (gu) => {
-  const szam = gu;
+const fillGuessedCode = (gueCo) => {
+  const num = gueCo;
   for (let j = 0; j < 4; j++) {
-    ground.playboard[1][j] = szam[j];
+    ground.playboard[1][j] = num[j];
   }
 };
 
